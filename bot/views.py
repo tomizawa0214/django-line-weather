da0fd260a99ab475a75d9b0b3ca57a82155e23ca
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-11-07 08:26:57 +0900

    scrape

--- bot/views.py
+++ bot/views.py
@@ -46,42 +46,52 @@ class CallbackView(View):
 
     @staticmethod
     @handler.add(MessageEvent, message=TextMessage)
-    # def handle_message(event):
-    #     dt = datetime.datetime.now()
-    #     if dt.hour == 14:
-    #         line_bot_api.reply_message(
-    #             event.reply_token,
-    #             [
-    #                 TextSendMessage(text='位置情報を教えてください。'),
-    #                 TextSendMessage(text='https://line.me/R/nv/location/')
-    #             ]
-    #         )
+    def handle_message(event):
+        text = event.message.text
+        if '位置情報' in text or '明日' in text or '翌日' in text or '天気' in text:
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text='URLをクリックして現在地を教えて\uDBC0\uDC2F'),
+                    TextSendMessage(text='https://line.me/R/nv/location/')
+                ]
+            )
 
-    def main():
-        r = requests.get('https://weather.tsukumijima.net/api/forecast/city/100010')
-        r_data = r.json()
-        d = r_data['forecasts']
-        maebashi = d[1]['image']['title']
-        h_temperature = d[1]['temperature']['max']['celsius']
-        l_temperature = d[1]['temperature']['min']['celsius']
-        rainy_percent_0 = d[1]['chanceOfRain']['00-06']
-        rainy_percent_6 = d[1]['chanceOfRain']['06-12']
-        rainy_percent_12 = d[1]['chanceOfRain']['12-18']
-        rainy_percent_18 = d[1]['chanceOfRain']['18-24']
+    @handler.add(MessageEvent, message=LocationMessage)
+    def handle_location(event):
+        text = event.message.address
 
-        context = {
-            'r': r,
-            'r_data': r_data,
-            'd': d,
-            'maebashi': maebashi,
-            'h_temperature': h_temperature,
-            'l_temperature': l_temperature,
-            'rainy_percent_0': rainy_percent_0,
-            'rainy_percent_6': rainy_percent_6,
-            'rainy_percent_12': rainy_percent_12,
-            'rainy_percent_18': rainy_percent_18
-        }
+        result = sc.get_weather_from_location(text)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=result)
+        )
 
-        result = render_to_string('blog/text_template/weather.txt', context)
-        messages = TextSendMessage(text=result)
-        line_bot_api.broadcast(messages=messages)
+    # def main():
+    #     r = requests.get('https://weather.tsukumijima.net/api/forecast/city/100010')
+    #     r_data = r.json()
+    #     d = r_data['forecasts']
+    #     maebashi = d[1]['image']['title']
+    #     h_temperature = d[1]['temperature']['max']['celsius']
+    #     l_temperature = d[1]['temperature']['min']['celsius']
+    #     rainy_percent_0 = d[1]['chanceOfRain']['00-06']
+    #     rainy_percent_6 = d[1]['chanceOfRain']['06-12']
+    #     rainy_percent_12 = d[1]['chanceOfRain']['12-18']
+    #     rainy_percent_18 = d[1]['chanceOfRain']['18-24']
+
+    #     context = {
+    #         'r': r,
+    #         'r_data': r_data,
+    #         'd': d,
+    #         'maebashi': maebashi,
+    #         'h_temperature': h_temperature,
+    #         'l_temperature': l_temperature,
+    #         'rainy_percent_0': rainy_percent_0,
+    #         'rainy_percent_6': rainy_percent_6,
+    #         'rainy_percent_12': rainy_percent_12,
+    #         'rainy_percent_18': rainy_percent_18
+    #     }
+
+    #     result = render_to_string('blog/text_template/weather.txt', context)
+    #     messages = TextSendMessage(text=result)
+    #     line_bot_api.broadcast(messages=messages)
