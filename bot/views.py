4009b570e84bfb21cc089f6cdae4c1c7fa8a62d4
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-11-07 09:43:15 +0900

    スクレイピング

--- bot/views.py
+++ bot/views.py
@@ -10,10 +10,12 @@ from linebot.models import (
     MessageEvent,
     TextMessage,
     TextSendMessage,
+    LocationMessage
 )
+from bs4 import BeautifulSoup
 import requests
-import pprint
 import os
+import re
 
 
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
@@ -57,41 +59,45 @@ class CallbackView(View):
                 ]
             )
 
+    # 位置情報からその日の天気を返す
+    def get_weather_from_location(original_location):
+        # 住所の中から郵便番号を抽出する
+        location = re.findall('\d{3}-\d{4}', original_location)
+        # 1回目のスクレイピングでは住所を検索し、候補から取ってくる
+        url = "https://weather.yahoo.co.jp/weather/search/?p=" + location[0]
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        content = soup.find(class_="serch-table")
+        # 2回目のスクレイピングで用いるURLを得る
+        location_url = "http:" + content.find('a').get('href')
+        r = requests.get(location_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        content = soup.find(id='yjw_pinpoint_today').find_all('td')
+        info = []
+
+        for each in content[1:]:
+            info.append(each.get_text().strip('\n'))
+
+        # 時間
+        time = info[:8]
+        # 天気
+        weather = info[9:17]
+        # 気温
+        temperature = info[18:26]
+        # 上の3つの情報を合わせる
+        weather_info = [(time[i], weather[i], temperature[i]) for i in range(8)]
+
+        result = [('{0[0]}: {0[1]}, {0[2]}°C'.format(weather_info[i])) for i in range(8)]
+        result = ('{}\nの今日の天気は\n'.format(original_location) + '\n'.join(result) + '\nです。')
+
+        return result
+
     @handler.add(MessageEvent, message=LocationMessage)
     def handle_location(event):
         text = event.message.address
 
-        result = sc.get_weather_from_location(text)
+        result = scrape.get_weather_from_location(text)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=result)
         )
-
-    # def main():
-    #     r = requests.get('https://weather.tsukumijima.net/api/forecast/city/100010')
-    #     r_data = r.json()
-    #     d = r_data['forecasts']
-    #     maebashi = d[1]['image']['title']
-    #     h_temperature = d[1]['temperature']['max']['celsius']
-    #     l_temperature = d[1]['temperature']['min']['celsius']
-    #     rainy_percent_0 = d[1]['chanceOfRain']['00-06']
-    #     rainy_percent_6 = d[1]['chanceOfRain']['06-12']
-    #     rainy_percent_12 = d[1]['chanceOfRain']['12-18']
-    #     rainy_percent_18 = d[1]['chanceOfRain']['18-24']
-
-    #     context = {
-    #         'r': r,
-    #         'r_data': r_data,
-    #         'd': d,
-    #         'maebashi': maebashi,
-    #         'h_temperature': h_temperature,
-    #         'l_temperature': l_temperature,
-    #         'rainy_percent_0': rainy_percent_0,
-    #         'rainy_percent_6': rainy_percent_6,
-    #         'rainy_percent_12': rainy_percent_12,
-    #         'rainy_percent_18': rainy_percent_18
-    #     }
-
-    #     result = render_to_string('blog/text_template/weather.txt', context)
-    #     messages = TextSendMessage(text=result)
-    #     line_bot_api.broadcast(messages=messages)
