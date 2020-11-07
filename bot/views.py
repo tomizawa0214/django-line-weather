f7ad7595970c175c254c0281c1d1a55cf573e176
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-11-07 10:02:14 +0900

    テスト

--- bot/views.py
+++ bot/views.py
@@ -65,15 +65,18 @@ class CallbackView(View):
         location = re.findall('\d{3}-\d{4}', original_location)
         # 1回目のスクレイピングでは住所を検索し、候補から取ってくる
         url = "https://weather.yahoo.co.jp/weather/search/?p=" + location[0]
+        print(url)
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'html.parser')
         content = soup.find(class_="serch-table")
         # 2回目のスクレイピングで用いるURLを得る
         location_url = "http:" + content.find('a').get('href')
+        print(location_url)
         r = requests.get(location_url)
         soup = BeautifulSoup(r.text, 'html.parser')
         content = soup.find(id='yjw_pinpoint_today').find_all('td')
         info = []
+        print(info)
 
         for each in content[1:]:
             info.append(each.get_text().strip('\n'))
@@ -87,8 +90,8 @@ class CallbackView(View):
         # 上の3つの情報を合わせる
         weather_info = [(time[i], weather[i], temperature[i]) for i in range(8)]
 
-        result = [('{0[0]}: {0[1]}, {0[2]}°C'.format(weather_info[i])) for i in range(8)]
-        result = ('{}\nの今日の天気は\n'.format(original_location) + '\n'.join(result) + '\nです。')
+        result_info = [('{0[0]}: {0[1]}, {0[2]}°C'.format(weather_info[i])) for i in range(8)]
+        result = ('{}\nの今日の天気は\n'.format(original_location) + '\n'.join(result_info) + '\nです。')
 
         return result
 
